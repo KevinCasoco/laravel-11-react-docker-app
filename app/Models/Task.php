56aa7ce7230d65d5b11e074b6a3bf4b02057d7d7
KevinCasoco <andreikevincasoco@gmail.com>
2024-05-20 18:31:54 +0800

    create task api controller

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id', 'completed'];
+
+    protected $fillable = ['description', 'title'];
+
+    public function createTask($task){
+        return $this->create($task);
+    }
 }
