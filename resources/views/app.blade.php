18ff83123046fd6f8ea24aff6104a191afc4f1c7
KevinCasoco <andreikevincasoco@gmail.com>
2024-05-20 17:26:08 +0800

    api service ts config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel React</title>
 </head>
   @vite('resources/css/app.css')
 <body>
     @viteReactRefresh
+    {{-- choose what file to render in react js --}}
     {{-- @vite('resources/js/app.jsx') --}}
     @vite('resources/js/app.ts')
     <div id="root"></div>
 </body>
+    <script>
+        window.env = {
+            API_BASE_URL: '{{ env("API_BASE_URL") }}'
+        }
+    </script>
 </html>
